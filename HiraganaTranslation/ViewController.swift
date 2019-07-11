d1725c4c00df209dc6b4b240b4ed9b17de2c1373
YAMADA TAKASHI <yamadatakashi.work1203@gmail.com>
2019-07-11 11:00:31 +0900

    Initial Commit

---
+++ HiraganaTranslation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiraganaTranslation
+//
+//  Created by 山田卓史 on 2019/07/11.
+//  Copyright © 2019 TAKASHI YAMADA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
