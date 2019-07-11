eab7b3b5b8a1e05a539f7ee844373ff02fdc698c
YAMADA TAKASHI <yamadatakashi.work1203@gmail.com>
2019-07-11 16:18:04 +0900

    Add: クラス名を追加、日本語入力機能を実装
    -各クラスにclassを記述しました
    -WordInputViewに説明ラベル、テキストフィールド、変換ボタンを設置
    -WordInputViewControllerにWordInputViewを表示、Delegateを実装のみ(処理は未実装)

--- HiraganaTranslation/View/WordInputView.swift
+++ HiraganaTranslation/View/WordInputView.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+import UIKit
+
+protocol WordInputViewDelegate: class {
+    func wordInputView(wordEditting view: WordInputView, text: String)
+    func wordInputView(sendButtonDidTap view: WordInputView)
+}
+
+class WordInputView: UIView {
+    
+    private var label: UILabel!                 //説明
+    private var wordTextField: UITextField!     //タスク内容を入力するUITextField
+    private var sendButton: UIButton!           //送信ボタン
+    
+    //デリゲート
+    weak var delegate: WordInputViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //説明
+        label = UILabel()
+        label.text = "日本語文字列をひらがなに変換します"
+        addSubview(label)
+        
+        //入力フィールド
+        wordTextField = UITextField()
+        wordTextField.delegate = self
+        wordTextField.tag = 0
+        wordTextField.placeholder = "日本語の文章を入力してください"
+        addSubview(wordTextField)
+        
+        
+        //送信ボタン
+        sendButton = UIButton()
+        sendButton.setTitle("変換する", for: .normal)
+        sendButton.setTitleColor(UIColor.black, for: .normal)
+        sendButton.layer.borderWidth = 0.5
+        sendButton.layer.cornerRadius = 4.0
+        sendButton.addTarget(self, action: #selector(sendButtonTapped(_:)), for: .touchUpInside)
+        addSubview(sendButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let posX = bounds.origin.x + 30;
+        
+        //位置サイズ調整
+        label.frame = CGRect(x: posX,
+                             y: bounds.origin.y + 10,
+                             width: bounds.size.width - 60,
+                             height: 50.0)
+        
+        //位置サイズ調整
+        wordTextField.frame = CGRect(x: posX,
+                                     y: bounds.origin.y + 60,
+                                     width: bounds.size.width - 60,
+                                     height: 50.0)
+        
+        //位置サイズ調整
+        let sendButtonSize = CGSize(width: 100, height: 50)
+        sendButton.frame = CGRect(x: posX,
+                                  y: wordTextField.frame.maxY + 20,
+                                  width: sendButtonSize.width,
+                                  height: sendButtonSize.height)
+        
+    }
+    
+    @objc func sendButtonTapped(_ sender: UIButton) {
+        delegate?.wordInputView(sendButtonDidTap: self)
+    }
+}
+
+extension WordInputView: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        //親に送信
+        delegate?.wordInputView(wordEditting: self, text: textField.text ?? "")
+        
+        return true
+    }
+}
+
--- HiraganaTranslation/ViewController/WordInputViewController.swift
+++ HiraganaTranslation/ViewController/WordInputViewController.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import UIKit
+
+class WordInputViewController: UIViewController {
+    
+    fileprivate var wordInputView: WordInputView!
+    
+    fileprivate var viewModel: WordInputViewModel!
+    fileprivate var tableView: UITableView!
+    fileprivate var refreshControl: UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        wordInputView = WordInputView()
+        wordInputView.delegate = self
+        view.addSubview(wordInputView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        wordInputView.frame = CGRect(x: view.safeAreaInsets.left,
+                                      y: view.safeAreaInsets.top,
+                                      width: view.frame.size.width - view.safeAreaInsets.left - view.safeAreaInsets.right,
+                                      height: view.frame.size.height - view.safeAreaInsets.bottom)
+
+    }
+}
+
+extension WordInputViewController: WordInputViewDelegate {
+    
+    func wordInputView(wordEditting view: WordInputView, text: String) {
+        print("編集中:\(text)")
+    }
+    
+    func wordInputView(sendButtonDidTap view: WordInputView) {
+        print("送信！！")
+    }
+    
+    
+}
